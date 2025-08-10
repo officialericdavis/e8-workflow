a60c49f1e4e2161c2a6446556bbc020c8125e8eb
officialericdavis <eric@e8productions.com>
2025-08-09 21:29:09 -0400

    Update ClientProviders.tsx

--- app/providers/ClientProviders.tsx
+++ app/providers/ClientProviders.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import '../amplify-config';
 import { RolesProvider } from '@providers/RolesProvider';
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
