42e6b430cea0f7e530b5ae2ae29dec97f1d975b6
officialericdavis <eric@e8productions.com>
2025-08-10 03:51:23 +0000

    fix(amplify): remove region from Auth.Cognito (v6 typing)

--- app/amplify-config.ts
+++ app/amplify-config.ts
@@ -2,17 +2,15 @@
 
 import { Amplify } from 'aws-amplify';
 
-const region = process.env.NEXT_PUBLIC_AWS_REGION ?? '';
 const userPoolId = process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID ?? '';
 const userPoolClientId = process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID ?? '';
 
-const hasCognito = !!(region && userPoolId && userPoolClientId);
+const hasCognito = !!(userPoolId && userPoolClientId);
 
 if (hasCognito) {
   Amplify.configure({
     Auth: {
       Cognito: {
-        region,
         userPoolId,
         userPoolClientId,
       },
