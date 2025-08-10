01754a9409e4539e6e996ff4459a6706c0e59e64
officialericdavis <eric@e8productions.com>
2025-08-09 20:26:17 -0400

    Update amplify-config.ts

--- app/amplify-config.ts
+++ app/amplify-config.ts
@@ -1,4 +1,4 @@
 import { Amplify } from 'aws-amplify';
-import awsExports from '../aws-exports';
+import awsExports from '../aws-exports'; // adjust path if aws-exports.js is not in root
 
 Amplify.configure(awsExports);
